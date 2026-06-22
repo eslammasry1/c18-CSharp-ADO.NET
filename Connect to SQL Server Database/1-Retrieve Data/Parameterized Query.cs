b8d9aed140961f9b4f86fa3c45994ecd2587bc34
eslammasry1 <eslam.mostafa.omer@gmail.com>
2026-06-22 15:07:49 +0300

    Create Parameterized Query.cs

---
+++ Connect to SQL Server Database/1-Retrieve Data/Parameterized Query.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Data.SqlClient;
+internal class Program
+{
+static string ConnectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=sa123456;";
+static void PrintAllContacts(string FirstName1)
+{
+SqlConnection Connection = new SqlConnection(ConnectionString);
+string Query = "select * from Contacts where FirstName=@FirstName1";
+SqlCommand Command = new SqlCommand(Query, Connection);
+Command.Parameters.AddWithValue("@FirstName1", FirstName1);
+try
+{
+Connection.Open();
+SqlDataReader Reader = Command.ExecuteReader();
+while (Reader.Read())
+{
+int ContactID = (int)Reader["ContactID"];
+string FirstName = (string)Reader["FirstName"];
+string LastName = (string)Reader["LastName"];
+string Email = (string)Reader["Email"];
+string Phone = (string)Reader["Phone"];
+string Address = (string)Reader["Address"];
+int CountryID = (int)Reader["CountryID"];
+Console.WriteLine($"Contact ID: {ContactID}");
+Console.WriteLine($"First Name: {FirstName}");
+Console.WriteLine($"Last Name: {LastName}");
+Console.WriteLine($"Email: {Email}");
+Console.WriteLine($"Phone: {Phone}");
+Console.WriteLine($"Address: {Address}");
+Console.WriteLine($"Country ID: {CountryID}");
+Console.WriteLine();
+}
+Reader.Close();
+Connection.Close();
+}
+catch (Exception ex)
+{
+Console.WriteLine(ex.Message);
+}
+}
+static void Main(string[] args)
+{
+PrintAllContacts("Jane");
+Console.ReadLine();
+}
+}
