51d74ed8706e85734bbd48930ec7546bf7ad8fce
eslammasry1 <eslam.mostafa.omer@gmail.com>
2026-06-29 10:56:43 +0300

    Add DateAccessSettings class for database connection

---
+++ Connect to SQL Server Database/Project on 3 Tier Architecture/ContactsDataAccessLayer/DateAccessSettings.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ContactsDataAccessLayer
+{
+    internal class clsDateAccessSettings
+    {
+       public static string ConnectionString = @"Server=.;Database=ContactsDB;User Id=sa;Password=sa123456;";
+
+    }
+}
